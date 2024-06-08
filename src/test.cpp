82fd4ce4303fa9828216373c921a45a7a03a3a1c
wettestsock <evilsharkeater@gmail.com>
2024-06-08 19:14:13 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -5,10 +5,11 @@
 #include <bitset>
 
 #define N '\n'
+#define BITS(x) std::bitset<8>(x)
 
 
 int main(void){
-    std::cout << std::bitset<8>(3-4);
+    std::cout << BITS(3) << N << BITS(-4) << N <<  BITS(-1);
     test();
 
 

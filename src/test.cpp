fc84d90c1c73d902495bb7324bb2a8b61fb76869
wettestsock <evilsharkeater@gmail.com>
2024-06-08 18:52:01 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -8,7 +8,7 @@
 
 
 int main(void){
-    std::cout << (INT_MIN == -pow(2,31)) << N << (INT_MAX == pow(2,31));
+    std::cout << (INT_MIN == -(int)pow(2,31)) << N << (INT_MAX == (int)pow(2,31)) << N;
     test();
 
 

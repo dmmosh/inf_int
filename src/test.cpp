7bb7166e351d80a015cef30af78590927042af4c
wettestsock <evilsharkeater@gmail.com>
2024-06-08 19:59:30 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -15,6 +15,11 @@ inline constexpr T max(const T& input){
     return ~(-1 << (sizeof(input) * 8 - 1));
 }
 
+template <typename T>
+inline constexpr T min(const T& input){
+    return 1 << (sizeof(input) * 8);
+}
+
 /*
 if the last bit is a 1 (negative, substract) then use & operator
 if the last bit is 0

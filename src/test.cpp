0c87b4d3e026208cd32da2b7c4a21488db6cfa83
wettestsock <evilsharkeater@gmail.com>
2024-06-08 19:15:59 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -7,9 +7,13 @@
 #define N '\n'
 #define BITS(x) std::bitset<8>(x)
 
+/*
+if the last bit is a 1 (negative, substract) then use & operator
+if the last bit is 0
+*/
 
 int main(void){
-    std::cout << BITS(3) << N << BITS(4) << N <<  BITS(7);
+    std::cout << BITS(3) << N << BITS(5) << N <<  BITS(8);
     test();
 
 

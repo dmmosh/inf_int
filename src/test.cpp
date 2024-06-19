4bc60956154b47bfd94f5724326b56e28682e303
wettestsock <evilsharkeater@gmail.com>
2024-06-18 23:56:22 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -16,9 +16,9 @@ int main(void){
     //inf_int<int8_t> a = 1000;
 
     inf_int<int8_t> a;
-    a = 10000000;
+    a = 1000000000;
     //std::cout << BITS(a.get_buffer()) << N <<  a.value<int>() << N ;
 
-    std::cout << a.get_base<int>() << N << BITS(a.get_buffer()) << N << max<int8_t, int>(a) << N << a.value<int>() << N ;
+    std::cout << a.get_base<int>() << N << BITS(a.get_buffer()) << N << max<int8_t, int>(a) << N << a.value<long>() << N ;
 
 }

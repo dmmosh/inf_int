261d78973859d272da11051f2f6f092d16ccdde0
wettestsock <evilsharkeater@gmail.com>
2024-06-20 22:15:21 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -19,7 +19,7 @@ int main(void){
     //inf_int<int8_t> a = 1000;
 
     inf_int<int8_t> a;
-    a = 2;
+    a = 100;
 
     //a = 2000;
     //std::cout << BITS(a.get_buffer()) << N <<  a.value<int>() << N ;

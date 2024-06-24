fc8b164173ad6795c14725d46200d2700694c840
wettestsock <evilsharkeater@gmail.com>
2024-06-23 21:41:53 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -30,8 +30,8 @@ int main(void){
 
     //a = 2000;vd
     //std::cout << BITS(a.get_buffer()) << N <<  a.value<int>() << N ;
-    int8_t c = -127;
+    int8_t c = -120;
 
-    std::cout << (int)min(c) << N << valid_subtract<int8_t>(c, 10);
+    std::cout << (int)min(c) << N << valid_subtract<int8_t>(c, 1);
 
 }

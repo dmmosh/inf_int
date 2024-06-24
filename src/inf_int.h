ffaa939323757df6aecf707a39d0f505e43837f7
wettestsock <evilsharkeater@gmail.com>
2024-06-23 22:43:53 -0400

    nothing of note

--- src/inf_int.h
+++ src/inf_int.h
@@ -294,7 +294,7 @@ inf_int<T>& inf_int<T>::operator+(U value) {
     value = base_convert<U>(value, 2, out.get_base()); // converts bases from 2 to inf int's
 
     
-    out.buffer+=base_convert<U>(value, 2, out.get_base());
+    out.buffer+=base_convert<T>(value, 2, out.get_base());
 
     return out;
 }; 

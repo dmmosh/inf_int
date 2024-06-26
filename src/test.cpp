5622b34891f7216e97037f4dc3d23121e76bfd55
wettestsock <evilsharkeater@gmail.com>
2024-06-25 23:26:55 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -21,7 +21,7 @@ int main(void){
     //inf_int<int8_t> a = 1000;
 
     inf_int<int8_t> a = -2000000;
-    inf_int<int8_t> b = 100;
+    inf_int<int8_t> b = 38192;
 
 
     std::cout << a.info() << b.info();

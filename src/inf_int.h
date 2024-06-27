b418a0380559b2d1fd24205dee776cd636938963
wettestsock <evilsharkeater@gmail.com>
2024-06-26 21:09:57 -0400

    nothing of note

--- src/inf_int.h
+++ src/inf_int.h
@@ -116,6 +116,8 @@ constexpr T base_convert(T val, const T& base_cur, const T& base_new){
     else 
         i = sizeof(val)*8-1;
     
+    if (val <0)
+        return -1;
     
     while(i >= 0) {
         //std::cout << val << i;

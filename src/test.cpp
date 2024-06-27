e11cd28bb6533591d12eebd3fef550b8c9ab8dc0
wettestsock <evilsharkeater@gmail.com>
2024-06-26 21:05:50 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -28,7 +28,7 @@ int main(void){
     a +=b;
     std::cout << a.info() << valid::max<int8_t, int>(a) << N << valid::min<int8_t, int>(a) << N;
 
-    int8_t out = base_convert<int8_t>(200, 2,3);
+    int8_t out = base_convert<int8_t>(120, 2,3);
     std::cout << N << (int)out << N << BITS(out)<< N << BITS((int8_t)-1);
 
 

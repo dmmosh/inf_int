6c8935ead554e3eb117957ffafd9e169ecf48e6a
wettestsock <evilsharkeater@gmail.com>
2024-06-26 20:34:14 -0400

    nothing of note

--- src/inf_int.h
+++ src/inf_int.h
@@ -58,12 +58,14 @@ constexpr T base_convert(T val, const T& base_cur, const T& base_new){
          
     while(val => 0 && i >= 0) {
         //std::cout << val << i;
+        if ((1<<i) & val) {
         auto minus = std::pow(base_new, i);
         if(val-minus =>0){
             val-=minus;
             out += 1<<i;
         }
         i--;
+        }
     }
 
     return out;

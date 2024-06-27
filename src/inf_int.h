6ead50b0357d8d6828dc8b86075d0e6c9a006047
wettestsock <evilsharkeater@gmail.com>
2024-06-26 21:57:03 -0400

    nothing of note

--- src/inf_int.h
+++ src/inf_int.h
@@ -119,6 +119,7 @@ constexpr T base_convert(T val, const T& base_cur, const T& base_new){
     bool negative = false;
     if (val <0){
         val = -val; // makes the value negative
+        return val;
         negative = true;
         i--;
     }
@@ -133,7 +134,7 @@ constexpr T base_convert(T val, const T& base_cur, const T& base_new){
         i--;
     }
     if (negative){
-        out = -out -1;
+        out = -out;
     }
 
     return out;

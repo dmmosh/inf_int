6cfd5b6f825f6119b14172ce9a284725b64e1a0c
wettestsock <evilsharkeater@gmail.com>
2024-07-03 22:09:53 -0400

    nothing of note

--- src/inf_int.h
+++ src/inf_int.h
@@ -144,22 +144,23 @@ constexpr T base_convert(T val, const T& base_old, const T& base_new){
     // }
 
     while(val > 0 && i >=0){
-            int8_t cur = log_base<T>(std::pow(base_old, i), base_new); // bit index to insert
-            //  log base new value's actual value at a given index
-            /*
-            ex.
-            base 3 -> 2 with value 121
-            log(121, 2) = 6 (bit index 6 aka 2^6 aka 64)
 
-            base 2 -> with value 121
-            log(121, 3) = 4 (bit index 4 aka 3^4 aka 81)
+        int8_t cur = log_base<T>(std::pow(base_old, i), base_new); // bit index to insert
+        //  log base new value's actual value at a given index
+        /*
+        ex.
+        base 3 -> 2 with value 121
+        log(121, 2) = 6 (bit index 6 aka 2^6 aka 64)
+        base 2 -> with value 121
+        log(121, 3) = 4 (bit index 4 aka 3^4 aka 81)
+        then removes that bit from the value
+        */
+       return cur;
+        if (cur < sizeof(val)*8-1){ // bit index doesnt overflow
+            out |= cur; // adds the index
+            val ^= (1<<i); //removes the bit from the value
+        }
 
-            then removes that bit from the value
-            */
-            if (cur < sizeof(val)*8-1){ // bit index doesnt overflow
-                out |= cur; // adds the index
-                val ^= (1<<i); //removes the bit from the value
-            }
         i--;
     }   
 

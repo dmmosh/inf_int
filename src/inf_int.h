39ca9cedc35b19ae17acedc18c538607c0bfdaa8
wettestsock <evilsharkeater@gmail.com>
2024-07-03 22:49:23 -0400

    nothing of note

--- src/inf_int.h
+++ src/inf_int.h
@@ -160,7 +160,7 @@ constexpr T base_convert(T val, const T& base_old, const T& base_new){
             log(121, 3) = 4 (bit index 4 aka 3^4 aka 81)
             then removes that bit from the value
             */
-           std::cout << (int)cur << '\t';
+           std::cout << (int)cur << '\n';
             if (cur < sizeof(val)*8-1){ // bit index doesnt overflow
                 BIT_SET(out, cur);
                 BIT_CLEAR(val, i);

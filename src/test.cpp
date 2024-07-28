94788b126ccfee371fb1fa67ae12fed7e0befd0f
wettestsock <evilsharkeater@gmail.com>
2024-07-28 12:06:56 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -42,7 +42,7 @@ int main(void){
     int8_t in2_new_base = 4;
 
 
-    test out1 = {base_convert<int8_t>(in1.val, in1.base, in1_new_base), in1_new_base};
+    //test out1 = {base_convert<int8_t>(in1.val, in1.base, in1_new_base), in1_new_base};
     test out2 = {base_convert<int8_t>(in2.val, in2.base, in2_new_base), in2_new_base};
 
 

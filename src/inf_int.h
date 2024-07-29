a203fabfe27f9298456f6c5860fdb7f24fccc4d4
wettestsock <evilsharkeater@gmail.com>
2024-07-29 00:10:46 -0400

    nothing of note

--- src/inf_int.h
+++ src/inf_int.h
@@ -323,8 +323,8 @@ class inf_int{
             return out;
         }
 
-        uT temp_base = out.get_base();
-        T temp_buffer = out.buffer;
+        uT temp_base = std::max(out.get_base(), value.base);
+        T temp_buffer = ::base_convert<T>(out.buffer, out.get_base(), temp_base);
         U temp_val = ::base_convert<U>(value.buffer,value.base, temp_base);
 
         while(!valid::add(temp_buffer, temp_val)){

ee7544d760b91c7ea14370b6b67715f6d0d4e469
wettestsock <evilsharkeater@gmail.com>
2024-07-28 22:33:26 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -30,10 +30,10 @@ int main(void){
 
     // BASE CONVERSION TESTING
     test in1 = {60, 2}; // 81
-    test in2 = {0b01111001, 3}; // 90
+    test in2 = {0b01100001, 5}; // 90
 
     int8_t in1_new_base =6;
-    int8_t in2_new_base = 4;
+    int8_t in2_new_base = 6;
 
     //test out1 = {base_convert<int8_t>(in1.val, in1.base, in1_new_base), in1_new_base};
     test out2 = {base_convert<int8_t>(in2.val, in2.base, in2_new_base), in2_new_base};

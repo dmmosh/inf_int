7da037a922a88541007e2e50bb3032c8d290a400
wettestsock <evilsharkeater@gmail.com>
2024-08-01 00:43:51 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -50,7 +50,7 @@ int main(void){
     // debug(in1);
     // debug(out1); skmklc
 
-    inf_int<int8_t> a = 9589438983493;
+    inf_int<int8_t> a = 9089438983493;
 
     std::cout << N <<  a.info();
 

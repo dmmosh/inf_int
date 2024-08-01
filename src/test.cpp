71983b7e497c90587d7b7902f7bdd61ac44129ae
wettestsock <evilsharkeater@gmail.com>
2024-08-01 00:28:42 -0400

    nothing of note

--- src/test.cpp
+++ src/test.cpp
@@ -30,7 +30,7 @@ int main(void){
 
     // BASE CONVERSION TESTING
     test in1 = {60, 2}; // 81
-    test in2 = {0b00010000, 4}; // 90
+    test in2 = {0b00010111, 4}; // 90
 
     // underflow errors TODO: fix
     // 01111111 base 3 to base 4
